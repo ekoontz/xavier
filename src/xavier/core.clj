23fcf0d18031ffa740a88761ebcc67b045bc1c29
Eugene Koontz <ekoontz@hiro-tan.org>
2025-02-01 17:26:33 +0100

    cleanup

--- src/xavier/core.clj
+++ src/xavier/core.clj
@@ -1,14 +1,6 @@
 (ns xavier.core
   (:require [lock-key.core :as lock-key]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
-
-(defn foo []
-  (+ 42 1))
-
 ;; TODO: use environmental variable
 (def password "sUzIbwS6aEq1J0X6mLGh")
 
@@ -17,25 +9,6 @@
 
 (defn decrypt [encrypted-input]
   (lock-key/decrypt-from-base64 encrypted-input password))
-
-(defn demo []
-  (-> "hello how are you??" encrypt decrypt))
-
-
-;; lei dorme -> she sleeps
-
-(def question-and-answer-structure
-  ;; when the user GETs /question/751, they get back:
-  {:question-id 42
-   :question "she sleeps"
-   :targets ["lei dorme"]
-   :correct-prefix ""
-   :score 0})
-
-(def question-response
-  (-> question-and-answer-structure
-      (dissoc :targets)
-      (merge {:encrypted (encrypt (str question-and-answer-structure))})))
 
 (defn evaluate [user-input targets current-score]
   ;; if user-input is a prefix of any of the targets, increment score; otherwise, decrement score.
@@ -51,6 +24,9 @@
            ;; if user-input is a prefix of any of the targets, increment score:
            (if (seq correct-prefixes)
              {:score (inc current-score)
+              :correct? (not (empty? (remove false? (map (fn [target]
+                                                           (= target user-input))
+                                                         targets))))
               :correct-prefix (first correct-prefixes)}
 
              ;; otherwise, decrement score:
@@ -62,24 +38,33 @@
         ;; 1. get from the user input two params: :user-input and :encrypted
         {user-input :user-input
          encrypted :encrypted} user-request
-        
+
         ;; 2. decrypt the correct answers and score from the user's :encrypted param:
         {targets :targets
          score :score}
         (-> encrypted decrypt read-string)
-        
+
         ;; 3. evaluate the user's input against the correct answers:
         evaluation (evaluate user-input targets score)]
-    
+
     ;; create the response to be sent back to the user:
     (-> evaluation
         (merge
          {:encrypted (-> evaluation str encrypt)})
         (dissoc :targets))))
 
+;; testing
+
 (def initial-rr
   ["GET /question/751"
-   question-response])
+   (let [q-and-a {:question-id 42
+                  :question "she sleeps"
+                  :targets ["lei dorme"]
+                  :correct-prefix ""
+                  :score 0}]
+     (-> q-and-a
+         (dissoc :targets)
+         (merge {:encrypted (-> q-and-a str encrypt)})))])
 
 (def rr-pair-1
   (let [user-request
@@ -123,9 +108,45 @@
     [user-request
      (server-response user-request)]))
 
+(def rr-pair-7
+  (let [user-request
+        {:user-input "lei d"
+         :encrypted (-> rr-pair-6 second :encrypted)}]
+    [user-request
+     (server-response user-request)]))
+
+(def rr-pair-8
+  (let [user-request
+        {:user-input "lei do"
+         :encrypted (-> rr-pair-7 second :encrypted)}]
+    [user-request
+     (server-response user-request)]))
+
+(def rr-pair-9
+  (let [user-request
+        {:user-input "lei dor"
+         :encrypted (-> rr-pair-8 second :encrypted)}]
+    [user-request
+     (server-response user-request)]))
+
+(def rr-pair-10
+  (let [user-request
+        {:user-input "lei dorm"
+         :encrypted (-> rr-pair-9 second :encrypted)}]
+    [user-request
+     (server-response user-request)]))
+
+(def rr-pair-11
+  (let [user-request
+        {:user-input "lei dorme"
+         :encrypted (-> rr-pair-10 second :encrypted)}]
+    [user-request
+     (server-response user-request)]))
+
+
 (def requests-and-responses
   (->>
-   [initial-rr rr-pair-1 rr-pair-2 rr-pair-3 rr-pair-4 rr-pair-5 rr-pair-6]
+   [initial-rr rr-pair-1 rr-pair-2 rr-pair-3 rr-pair-4 rr-pair-5 rr-pair-6 rr-pair-7 rr-pair-8 rr-pair-9 rr-pair-10 rr-pair-11]
    (map (fn [x]
           {:request (first x)
            :response (second x)}))))
